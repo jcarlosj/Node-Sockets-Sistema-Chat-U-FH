7480a95bbfbdc31286a880a6cc293e886b4099e7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-29 09:51:28 -0500

    [+] Class > Users: Definición

---
+++ server/classes/users.js
@@ -0,0 +1,39 @@
+/** Clase */
+class Users {
+    constructor() {
+        this .users = [];   // Usuarios conectados al Chat
+    }
+
+    add( id, username ) {
+        let newuser = { id, username };
+
+        this .users .push( newuser );
+
+        return this .users;
+    }   
+
+    getById( id ) {
+        let user = this .users .filter( user => user .id === id )[ 0 ];   // Regresa solo al usuario cuyo 'id' sea igual al 'id' solicitado. Como filter retorna un Array le diremos que solo queremos el resultado del primero es decir el campo CERO
+
+        return user;
+    }
+
+    getAll() {
+        return this .users;
+    }
+
+    delete( id ) {
+        let userDeleted = this .getUserById( id );
+
+        this .users = this .users .filter( user => {
+            return user .id != id;      // Regresa todos los usuarios diferentes al 'id' solicitado
+        });
+
+        return userDeleted;
+    }
+}
+
+/** Exporta */
+module .exports = {
+    Users
+}
