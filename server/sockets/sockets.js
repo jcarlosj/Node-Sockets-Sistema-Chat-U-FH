80d27a51a8092049f45bc9e3b7d0dfd7b334c53a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-29 10:11:59 -0500

    [+] Sockets > Client/Server: Comunicación Básica

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -3,4 +3,17 @@ const { io } = require( '../server' );
 /** Escucho en el evento de conexión del Cliente. */
 io .on( 'connection', ( client ) => {
     console .log( 'Usuario conectado al Servidor' );    // Registro en la consola la conexión del Socket al Servidor
+
+    /** Escucha evento de desconección del Cliente */
+    client .on( 'disconnect', () => {
+        console .log( 'Usuario desconectado!' );
+    });
+
+    /** Emite un mensaje al Cliente  */
+    client .emit( 'userData', {         // 'userData' Nombre del evento que identifica el mensaje a Emitir
+            user: 'anonymous',              // Datos Enviados a través del Socket (Generalmente en formato JSON)
+            message: 'Bienvenido al Chat'
+        } 
+    );
+    
 });
